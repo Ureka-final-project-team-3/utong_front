b5b69d592d332fd0d57dda0f38efe055b4a5961e
Youdongseok <rex010307@gmail.com>
2025-07-06 19:56:04 +0900

    feat : 네비게이션 바 구현

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -5,10 +5,13 @@ import NavigationBar from '../components/NavigationBar/NavigationBar';
 const DefaultLayout = () => {
   return (
     <div className="w-screen h-screen bg-gray-200 flex items-center justify-center">
-      <div className="w-[360px] h-[780px] bg-white  shadow-xl flex flex-col overflow-hidden">
-        <div className="flex-1 overflow-y-auto">
+      <div className="w-[360px] h-[780px] bg-white shadow-xl flex flex-col overflow-hidden">
+        {/* 정확히 네비게이션 바 높이(85px)를 제외한 Outlet 영역 */}
+        <div className="[height:calc(780px-85px)] overflow-y-auto px-[30px] pt-[55px] pb-[30px] relative">
           <Outlet />
         </div>
+
+        {/* 네비게이션 바 (고정 85px) */}
         <NavigationBar />
       </div>
     </div>
