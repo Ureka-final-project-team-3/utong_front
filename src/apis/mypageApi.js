fdfafa15157ef62da0b4076801319f70ed71923f
gusdn6288 <gusdn-2137@naver.com>
2025-07-18 21:35:40 +0900

    fix:api 경로수정

--- src/apis/mypageApi.js
+++ src/apis/mypageApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/mypage',
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/user',
   withCredentials: true,
 });
 
