718865262f5e0005aa84573cfe7350e7ee090813
chemnida <chaemin9073@gmail.com>
2025-07-23 10:17:34 +0900

    fix : 안내모달 제거

--- src/pages/EventPage/components/EventHeader.jsx
+++ src/pages/EventPage/components/EventHeader.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from 'react'
-import BackButton from '../../../components/BackButton/BackButton'
-import help from '@/assets/icon/help.svg';
-import EventInfoModal from './EventInfoModal';
-
+import React, { useState } from 'react';
+import BackButton from '../../../components/BackButton/BackButton';
+// import help from '@/assets/icon/help.svg';
+// import EventInfoModal from './EventInfoModal';
 
 const EventHeader = () => {
-     const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-    <div className="w-full   flex items-center justify-between">
-      <BackButton />
+      <div className="w-full relative flex items-center justify-between h-[50px]">
+        <div className="absolute left-0">
+          <BackButton />
+        </div>
 
-      <h1 className="text-[length:var(--text-lg)] text-[color:var(--gray-800)] font-bold">
-        룰렛 이벤트
-      </h1>
+        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-[length:var(--text-lg)] text-[color:var(--gray-800)] font-bold">
+          룰렛 이벤트
+        </h1>
 
-      <img
-                src={help}
-                alt="도움말"
-                draggable={false}
-                className="cursor-pointer"
-                onClick={() => setShowModal(true)}
-              />
-    </div>
-     {showModal && <EventInfoModal onClose={() => setShowModal(false)} color="blue" />}
+        <div className="absolute right-0 flex items-center">
+          {/* <img
+            src={help}
+            alt="도움말"
+            draggable={false}
+            className="cursor-pointer"
+            onClick={() => setShowModal(true)}
+          /> */}
+          <div className="w-[20px]"></div>
+        </div>
+      </div>
+
+      {/* {showModal && <EventInfoModal onClose={() => setShowModal(false)} color="blue" />} */}
     </>
-  )
-}
+  );
+};
 
 export default EventHeader;
