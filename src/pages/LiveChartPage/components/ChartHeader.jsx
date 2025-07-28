f7347b56825aed6b98ede0fd97d2976c65aabc6c
Yudongseok <rex010307@gmail.com>
2025-07-28 17:56:20 +0900

    feat:네비게이션 추가

--- src/pages/LiveChartPage/components/ChartHeader.jsx
+++ src/pages/LiveChartPage/components/ChartHeader.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 import BackButton from '../../../components/BackButton/BackButton';
 
 import help from '@/assets/icon/help.svg';
+import { useNavigate } from 'react-router-dom';
 
 const ChartHeader = () => {
+  const navigate = useNavigate();
+
+  const goTotradeguide = () => {
+    navigate(`/tradeguide`);
+  };
+
   return (
     <div className="w-full   flex items-center justify-between">
       <BackButton />
-
       <h1 className="text-[length:var(--text-lg)] text-[color:var(--gray-800)] font-bold">
         데이터 거래하기
       </h1>
-
-      <img src={help} alt="TradeGuide" draggable={false} />
+      <img src={help} alt="TradeGuide" draggable={false} onClick={goTotradeguide} />
     </div>
   );
 };
